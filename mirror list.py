dcf3f406aa5766efe79b7a645c6015580e05c825
khoshsoroorm <khoshsoroorm@gmail.com>
2019-06-03 22:00:49 +0430

    mirror list right and left DONE

--- mirror list.py
+++ mirror list.py
@@ -1,4 +1,13 @@
 word_1 = 'Right>>>'
 word_2 = 'Left>>>>'
-w_list = word_1.split()
-print(w_list)
+w1 = word_1.replace('>', '<')
+w1_list = list(w1)
+w1_listjoin = w1_list[::-1]
+w1join = ''.join(w1_listjoin)
+w2 = word_2.replace('>', '<')
+w2_list = list(w2)
+w2_listjoin = w2_list[::-1]
+w2join = ''.join(w2_listjoin)
+print(word_2, ' | ', word_1)
+print(w1join, ' | ', word_1)
+print(word_2 , ' | ', w2join)
